7f3006836e3b79197d0d4f725612c1bac2f78dbd
GiorgiChkhitunidze <112623714+GiorgiChkhitunidze@users.noreply.github.com>
2023-05-22 23:31:35 +0400

    update

--- App/static/script.js
+++ App/static/script.js
@@ -7,17 +7,15 @@ function preview(){
     numOfFiles.textContent = ``;
     numOfFiles.style.margin = '0'
 
-    for(i of fileInput.files){
-        let reader = new FileReader();
-        let figure = document.createElement("figure");
-        let figCap = document.createElement("figcaption");
-        figure.appendChild(figCap);
-        reader.onload=()=>{
-            let img = document.createElement("img");
-            img.setAttribute("src",reader.result);
-            figure.insertBefore(img,figCap);
-        }
-        imageContainer.appendChild(figure);
-        reader.readAsDataURL(i);
+    let reader = new FileReader();
+    let figure = document.createElement("figure");
+    let figCap = document.createElement("figcaption");
+    figure.appendChild(figCap);
+    reader.onload=()=>{
+        let img = document.createElement("img");
+        img.setAttribute("src",reader.result);
+        figure.insertBefore(img,figCap);
     }
+    imageContainer.appendChild(figure);
+    reader.readAsDataURL(fileInput.files[0]);
 }
